ff41d3e579c1d06e4916afa71f0e0f6144d2485d
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-09-02 02:39:38 +0530

    added ec2 instance

---
+++ terraform/ec2.tf
@@ -0,0 +1,59 @@
+# SSM for EC2
+# IAM role assumed by EC2
+resource "aws_iam_role" "ssm" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ssm.name
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type          = "t3.small"
+  subnet_id              = module.vpc.private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm.name
+  user_data              = <<-EOF
+                yum update -y
+                yum install -y docker
+
+                systemctl start docker
+                systemctl enable docker
+
+                usermod -aG docker ec2-user
+                EOF
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
